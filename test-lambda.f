2fd5a487394fd78a81892027056895511d8de771
yhzhang0128 <zhangyunh@gmail.com>
2016-04-19 20:43:56 +0800

    Add Church Numeral test cases

--- test-lambda.f
+++ test-lambda.f
@@ -22,5 +22,19 @@ lambda x. lambda y. y;
 (lambda x. lambda y. x (lambda x. lambda y. x) y);
 (lambda x. lambda y. x (lambda x. lambda y. x) y) (lambda x. lambda y. y) (lambda x. lambda y. y);
 
+/* Church Numeral */
+/* 0 */
+(lambda s. lambda z. z);
+/* 1 */
+(lambda s. lambda z. s z);
+/* 2 */
+(lambda s. lambda z. s (s z));
+
+/* Numeral Multiplication */
+(lambda x. lambda y. lambda s. lambda z. x (y s) z);
+/* 0 * 1 = 0 */
+(lambda x. lambda y. lambda s. lambda z. x (y s) z) (lambda s. lambda z. z) (lambda s. lambda z. s z);
+
+
 /* Dangerous: Omega combinator */
 (lambda x.x x) (lambda x.x x);
